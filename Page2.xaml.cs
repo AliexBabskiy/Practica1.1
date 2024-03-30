15e9b8380c1ce6a790fd070ee1bfaf7ed3c89bf5
AliexBabskiy <ababskiy@gmail.com>
2024-03-30 10:14:36 +0300

    первая фиксация

---
+++ Page2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrPract1._1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        private BdPracticaEntities context = new BdPracticaEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            tablic.ItemsSource = context.Clients.ToList();
+        }
+    }
+}
